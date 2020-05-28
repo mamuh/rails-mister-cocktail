bccd4bf17cb3ec85714a5010574bc1d85bd18e9f
Mariana Caramori <musardo@mail.com>
2020-05-28 13:07:46 -0300

    cocktails and doses controllers generated

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :cocktail, uniqueness: { scope: :ingredient }
+  # 4.5g of mint on cocktail1. 2g of mint on cocktail2
   validates :description, presence: true
 end
